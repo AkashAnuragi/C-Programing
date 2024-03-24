464e4aa6c1a45abb68916c8c3c0fa87bbe73a38d
AKASH ANURAGI <136264753+akdeveloper01@users.noreply.github.com>
2024-03-24 21:27:16 +0530

    2

---
+++ day 2.c
@@ -0,0 +1,16 @@
+void main()
+{
+/*Addition, subtraction, division,multiplication*/
+int a,b,sum,sub,div,mul;
+a=10;
+b=2;
+sum=a+b;
+sub=a-b;
+div=a/b;
+mul=a*b;
+printf("\nsum is :%d",sum);
+printf("\nsub is :%d",sub);
+printf("\ndiv is :%d",div);
+printf("\nmul is :%d",mul);
+getch();
+}
