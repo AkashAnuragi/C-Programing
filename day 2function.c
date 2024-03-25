01c38fc0a28dca62e66df7d93d829c2a9cc56ac9
AKASH ANURAGI <136264753+akdeveloper01@users.noreply.github.com>
2024-03-25 22:44:49 +0530

    3

---
+++ day 2function.c
@@ -0,0 +1,15 @@
+ak()
+{
+printf("\nHey Akash");
+int a,b,c;
+printf("Enter the numbers");
+scanf("%d%d",&a,&b);
+c=a+b;
+printf("\nAddition is %d:",c);
+}
+main()
+{
+printf("hey Anuragi");
+ak(); /*this is calling the function*/
+getch();
+}
