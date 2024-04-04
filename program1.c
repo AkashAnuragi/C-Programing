351f48328da3be48a304beffc3303e638de2ffab
AKASH ANURAGI <136264753+akdeveloper01@users.noreply.github.com>
2024-04-04 21:28:29 +0530

    12

---
+++ program1.c
@@ -0,0 +1,15 @@
+//WAP to add,sub,div,mul of two number the value is given by user?
+main(){
+int a,b,c,d,e,f;
+printf("enter the value : ");
+scanf(" %d %d",&a ,&b);
+c =a+b;
+d=a-b;
+e=a/b;
+f=a*b;
+printf("\nsum is=%d",c);
+printf("\nsub is=%d",d);
+printf("\ndiv is=%d",e);
+printf("\nmul is=%d",f);
+getch();
+}
