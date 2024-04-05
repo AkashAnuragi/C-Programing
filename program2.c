9a847edb09edb07703286793cd993cd69a839af5
AKASH ANURAGI <136264753+akdeveloper01@users.noreply.github.com>
2024-04-05 23:01:37 +0530

    13

---
+++ program2.c
@@ -0,0 +1,10 @@
+//WAP to find the are a circle
+main()
+{
+float area , r;
+printf("Enter the Radius of Circle : ");
+scanf("%f",&r);
+area=3.14*r*r;
+printf("\n Area of Circle is : %f",area);
+getch();
+}
