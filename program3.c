ecbb50628887db7eed3de11026e538e1995d6a19
AKASH ANURAGI <136264753+akdeveloper01@users.noreply.github.com>
2024-04-09 23:11:29 +0530

    Add files via upload

---
+++ program3.c
@@ -0,0 +1,14 @@
+//WACP to check whether the given number is  even or odd .
+void main ()
+{
+int n ,fac;
+printf("Enter the value : ");
+scanf("%d",&n);
+if( n%2 == 0.0){
+printf(" %d is the even number",n);
+}
+else{
+printf("%d is the odd number",n);
+}
+getch();
+}
